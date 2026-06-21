2f9ed06e1bcb1bdc06b8e886dd439e6713d2a9ed
yahel <ozeriyahel@gmail.com>
2026-06-21 13:53:49 +0300

    Add configuration file for server settings and content types

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+HOST = "0.0.0.0"
+PORT = 8080
+BUFFER_SIZE = 4096
+STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+DEFAULT_PAGE = "/index.html"
+HTTP_VERSION = "HTTP/1.0"
+
+CONTENT_TYPES = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".gif": "image/gif",
+    ".txt": "text/plain",
+}
+DEFAULT_CONTENT_TYPE = "application/octet-stream"
